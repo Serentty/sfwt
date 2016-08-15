36ae30224ea3bf8d594630830230617ad55ead5b
pixelsharmana <pixel_sharmana@hotmail.com>
2016-08-15 17:41:02 -0400

    Helps with finding orbital data
    A bunch of simple but essential math formulas used for ellipses

---
+++ Ellipse.h
@@ -0,0 +1,9 @@
+#ifndef ELLIPSE_H
+#define ELLIPSE_H
+
+double findDistanceBetweenPoints(double aX, double aY, double bX, double bY);
+double findFoci(double semiMajorAxis, double semiMinorAxis);
+double findEccentricity(double semiMajorAxis, double semiMinorAxis);
+
+
+#endif
