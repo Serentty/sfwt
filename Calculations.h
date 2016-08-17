b55d1f4606c1860934582eb02d9d898c2ceb53be
Gareth Poole <noname422@gmail.com>
2016-08-17 15:07:55 -0400

    Remove duplicate files

--- Calculations.h
+++
@@ -1,9 +0,0 @@
-#ifndef CALCULATIONS_H
-#define CALCULATIONS_H
-
-#include "Material.h"
-
-double findMass(Material material, double volume);
-double findVolume(Material material, double mass);
-
-#endif
