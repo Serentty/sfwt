f7939c1607141ac270e3af4ff5e23054e12010de
pixelsharmana <pixel_sharmana@hotmail.com>
2016-08-18 23:30:37 -0400

    Find an intersection between an ellipse and a line
    It's the most ugly bit of code ever

--- src/Ellipse.cpp
+++ src/Ellipse.cpp
@@ -2,10 +2,11 @@
 #include "Vector.h"
 
 const double GRAVITATIONAL_CONSTANT = 6.674e-11;
+const double PI = 3.141592;
 
 double findDistanceBetweenPoints(double aX, double aY, double bX, double bY)
 {
-    return std::sqrt(std::pow((bX - aX), 2) + std::pow((bY - aY), 2));
+    return sqrt(pow((bX - aX), 2) + pow((bY - aY), 2));
 }
 
 double findFoci(double semiMajorAxis, double semiMinorAxis)
@@ -16,7 +17,7 @@ double findFoci(double semiMajorAxis, double semiMinorAxis)
         semiMajorAxis = semiMinorAxis;
         semiMinorAxis = temp;
     }
-    return std::sqrt(std::pow(semiMajorAxis, 2) - std::pow(semiMinorAxis, 2));
+    return sqrt(pow(semiMajorAxis, 2) - pow(semiMinorAxis, 2));
 }
 
 // eccentricity = distance from the center to a focus Divided by the distance from that focus to a vertex
@@ -43,14 +44,30 @@ double findOrbitalPeriod(double semiMajorAxis)
     //Re : Mean distance between Earth and Sun //1.4960e11m
     //Tp : Orbital period of planet
     //Rp : Mean distance of Planet (Semi Major Axis)
-    return 31536000 * std::pow((semiMajorAxis / 1.4960e11), 1.5);
+    return 31536000 * pow((semiMajorAxis / 1.4960e11), 1.5);
+}
+
+double findTheta(double x, double y){
+    if (x==0 && y==0)
+        return 0;
+	double theta;
+	theta = atan(y / x);
+	theta = (theta * 180) / PI;
+	return theta;
 }
 
 double findSemiMinorAxis(double semiMajorAxis, double eccentricity){
-    return semiMajorAxis * std::sqrt(1 - std::pow(eccentricity,2.0));
+    return semiMajorAxis * sqrt (1 - pow(eccentricity,2.0));
 }
 
-double findEllipseAndLineIntersection(double SemiMajorAxis, double eccentricity, double theta)
+Vector2D findEllipseAndLineIntersection(double semiMajorAxis, double eccentricity, double theta, double lineXDisplacement, double ellipseXDisplacement, double ellipseYDisplacement)
 {
-
+    //Ellipse a = SemiMajorAxis
+    //Ellipse b = semiminor
+    double lineRate = sin(theta)/cos(theta);
+    double semiMinorAxis = findSemiMinorAxis(semiMajorAxis,eccentricity);
+    Vector2D point;
+    point.x = ((ellipseXDisplacement*pow(semiMinorAxis,2))-(lineRate*(pow(semiMajorAxis,2))*(lineXDisplacement-ellipseYDisplacement))+semiMajorAxis*semiMinorAxis*sqrt(((pow(semiMajorAxis,2))*(pow(lineRate,2)))+pow(semiMinorAxis,2)-pow((lineXDisplacement+lineRate*ellipseXDisplacement),2)-pow(ellipseYDisplacement,2)+(2*(lineXDisplacement+lineRate*ellipseXDisplacement)*ellipseYDisplacement)))/((pow(semiMajorAxis,2))*(pow(lineRate,2))+pow(semiMinorAxis,2));
+    point.y = (pow(semiMinorAxis,2)*(lineXDisplacement+lineRate*ellipseXDisplacement))+ellipseYDisplacement*pow(semiMajorAxis,2)*pow(lineRate,2)+semiMajorAxis*semiMinorAxis*lineRate*sqrt(pow(semiMajorAxis,2)*pow(lineRate,2)+pow(semiMinorAxis,2)-pow((lineXDisplacement+lineRate*ellipseXDisplacement),2)-pow(ellipseYDisplacement,2)+(2*(lineXDisplacement+lineRate*ellipseXDisplacement)))/((pow(semiMajorAxis,2)*pow(lineRate,2))+pow(semiMinorAxis,2));
+    return point;
 }
