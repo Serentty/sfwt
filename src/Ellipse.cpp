8170517cd8feb3d6ed2458875415ab830d7fec78
Gareth Poole <noname422@gmail.com>
2016-08-18 23:07:41 -0400

    Add std:: before function calls in Ellipse.cpp

--- src/Ellipse.cpp
+++ src/Ellipse.cpp
@@ -6,7 +6,7 @@ const double PI = 3.141592;
 
 double findDistanceBetweenPoints(double aX, double aY, double bX, double bY)
 {
-    return sqrt(pow((bX - aX), 2) + pow((bY - aY), 2));
+    return std::sqrt(std::pow((bX - aX), 2) + std::pow((bY - aY), 2));
 }
 
 double findFoci(double semiMajorAxis, double semiMinorAxis)
@@ -17,7 +17,7 @@ double findFoci(double semiMajorAxis, double semiMinorAxis)
         semiMajorAxis = semiMinorAxis;
         semiMinorAxis = temp;
     }
-    return sqrt(pow(semiMajorAxis, 2) - pow(semiMinorAxis, 2));
+    return std::sqrt(std::pow(semiMajorAxis, 2) - std::pow(semiMinorAxis, 2));
 }
 
 // eccentricity = distance from the center to a focus Divided by the distance from that focus to a vertex
@@ -44,11 +44,11 @@ double findOrbitalPeriod(double semiMajorAxis)
     //Re : Mean distance between Earth and Sun //1.4960e11m
     //Tp : Orbital period of planet
     //Rp : Mean distance of Planet (Semi Major Axis)
-    return 31536000 * pow((semiMajorAxis / 1.4960e11), 1.5);
+    return 31536000 * std::pow((semiMajorAxis / 1.4960e11), 1.5);
 }
 
 double findSemiMinorAxis(double semiMajorAxis, double eccentricity){
-    return semiMajorAxis * sqrt (1 - pow(eccentricity,2.0));
+    return semiMajorAxis * std::sqrt(1 - std::pow(eccentricity,2.0));
 }
 
 double findEllipseAndLineIntersection(double SemiMajorAxis, double eccentricity, double theta)
