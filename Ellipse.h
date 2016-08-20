d819c9743492e52910b539f3255874de9c3a5fda
pixelsharmana <pixel_sharmana@hotmail.com>
2016-08-19 23:07:50 -0400

    Fast line ellipse intersection
    By supposing that the Ellipse's center is 0,0 and that the rayi's deviation is centered on the Ellipse, we can greatly simplify the math involved

---
+++ Ellipse.h
@@ -0,0 +1,17 @@
+#ifndef ELLIPSE_H
+#define ELLIPSE_H
+
+#include "Vector.h"
+
+double findDistanceBetweenPoints(double aX, double aY, double bX, double bY);
+double findFoci(double semiMajorAxis, double semiMinorAxis);
+double findEccentricity(double semiMajorAxis, double semiMinorAxis);
+double findOrbitalPeriod(double semiMajorAxis);
+double findTheta(double x, double y);
+double findSemiMinorAxis(double semiMajorAxis, double eccentricity);
+Vector2D findFastEllipseAndLineIntersection(double semiMajorAxis, double eccentricity, double theta);
+Vector2D findEllipseAndLineIntersection(double semiMajorAxis, double eccentricity, double theta,
+                                        double lineXDisplacement, double ellipseXDisplacement,
+                                        double ellipseYDisplacement);
+
+#endif
