edb30d8102d1813003a2542a4d4c11f6526f5f66
Gareth Poole <noname422@gmail.com>
2016-08-19 21:44:26 -0400

    Real-time demo of findDistance()

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ int main()
     raw();
     keypad(stdscr, TRUE);
     noecho();
+    timeout(0);
     attron(A_BOLD);
     printw("Space Fortress");
     attroff(A_BOLD);
@@ -23,18 +24,25 @@ int main()
     ch.tiles[4][4][15].attributes[0] = true;
     Animal player;
     player.character = '@';
+    Animal dude;
+    dude.character = '&';
     player.mass = 60;
     player.location.x = 2.0;
     player.location.y = 2.0;
     player.location.z = 15.0;
+    dude.mass = 40;
+    dude.location.x = 4.0;
+    dude.location.y = 4.0;
+    dude.location.z = 15.0;
     ch.entities.push_back(&player);
+    ch.entities.push_back(&dude);
     int key = 0;
     int ticks = 0;
     for(;;)
     {
         key = getch();
-        ch.run(100);
-        ticks += 100;
+        ch.tick();
+        ticks++;
         switch(key)
         {
             case KEY_UP: // up
@@ -54,12 +62,16 @@ int main()
                 return 0;
                 break;
         }
-        clear();
-        printw(render(ch).c_str());
-        printw("TIME ELAPSED: %f SECONDS\n", ticks / 1000.0);
-        printw("SPEED: %f METRES / SECOND\n", player.findSpeed());
-        printw("KINETIC ENERGY: %f JOULES", player.findKineticEnergy());
-        refresh();
+        if(ticks % 1000 == 0)
+        {
+            clear();
+            printw(render(ch).c_str());
+            printw("TIME ELAPSED: %f SECONDS\n", ticks / 1000.0);
+            printw("SPEED: %f METRES / SECOND\n", player.findSpeed());
+            printw("KINETIC ENERGY: %f JOULES\n", player.findKineticEnergy());
+            printw("DISTANCE FROM DUDE: %f METRES", player.location.findDistance(dude.location));
+            refresh();
+        }
     }
     getch();
     endwin();
